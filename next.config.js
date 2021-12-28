c7ade438d27d39dcf44a42e3a96e038f3812d452
carloslavie <carloslavie@gmail.com>
2021-12-28 16:15:03 -0300

    change next config urls4

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   env:{
-    backendURL: 'https://calm-island-98313.herokuapp.com/',
+    backendURL: 'https://calm-island-98313.herokuapp.com',
     frontendURL: 'https://nodesend-cliente-weld-five.vercel.app'
   }
 }
